7378b3dff5682d58b280de4854b72e6147c80998
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-14 18:19:56 -0600

    db:migrate strain_effect table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_230927) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_233359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_230927) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "strain_effects", force: :cascade do |t|
+    t.bigint "strain_id", null: false
+    t.bigint "effect_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["effect_id"], name: "index_strain_effects_on_effect_id"
+    t.index ["strain_id"], name: "index_strain_effects_on_strain_id"
   end
 
   create_table "strains", force: :cascade do |t|
@@ -39,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_230927) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "strain_effects", "effects"
+  add_foreign_key "strain_effects", "strains"
   add_foreign_key "strains", "types"
 end
