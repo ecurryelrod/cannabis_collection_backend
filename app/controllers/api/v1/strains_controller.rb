816dd0a2ba79abced81891a41e40d3e19d14fba6
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-15 16:42:24 -0600

    deleted comments from strains controller

--- app/controllers/api/v1/strains_controller.rb
+++ app/controllers/api/v1/strains_controller.rb
@@ -1,19 +1,16 @@
 class Api::V1::StrainsController < ApplicationController
   before_action :set_strain, only: %i[ show update destroy ]
 
-  # GET /strains
   def index
     @strains = Strain.all
 
     render json: @strains
   end
 
-  # GET /strains/1
   def show
     render json: @strain
   end
 
-  # POST /strains
   def create
     @strain = Strain.new(strain_params)
 
@@ -24,7 +21,6 @@ class Api::V1::StrainsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /strains/1
   def update
     if @strain.update(strain_params)
       render json: @strain
@@ -33,18 +29,15 @@ class Api::V1::StrainsController < ApplicationController
     end
   end
 
-  # DELETE /strains/1
   def destroy
     @strain.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_strain
       @strain = Strain.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def strain_params
       params.require(:strain).permit(:name, :description, :terpene, :thc_amount, :cbd_amount, :cbg_amount, :type_id)
     end
