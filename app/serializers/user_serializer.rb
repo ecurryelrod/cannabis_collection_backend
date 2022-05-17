e0799656aaa073d6d8bcdeaee91aaf4dceea8954
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-17 14:46:38 -0600

    updated UserSerializer with jsonapi and UsersController renders

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,24 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password_digest
+# class UserSerializer < ActiveModel::Serializer
+#   attributes :id, :email, :password_digest
+# end
+
+class UserSerializer
+  include JSONAPI::Serializer
+
+  attributes :email
+  # has_many :strains
+
+  attribute :strains do |type|
+    type.strains.map do |strain| 
+        {
+          id: strain.id,
+          name: strain.name,
+          description: strain.description,
+          terpene: strain.terpene,
+          thc_amount: strain.thc_amount,
+          cbd_amount: strain.cbd_amount,
+          cbg_amount: strain.cbg_amount
+        }
+    end
+  end
 end
