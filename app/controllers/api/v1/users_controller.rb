f5b950a1005088dad82f7b86af4e7725e0c67e38
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-17 15:17:35 -0600

    UsersController user_params password update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -46,6 +46,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :password_digest)
+      params.require(:user).permit(:email, :password)
     end
 end
