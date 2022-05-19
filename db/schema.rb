ff80cefe9cd81305df1fc3f2dcf3bc24ea60f901
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-19 16:33:31 -0600

    added name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_203111) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_223243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_203111) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "strain_effects", "effects"
