94c83a984a6e4220f890cdab24d6e76d2605b329
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-22 11:18:02 -0600

    added current_user and logged_in? helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_id?
+        !!current_user
+    end
 end
