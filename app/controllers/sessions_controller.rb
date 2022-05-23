965df064f8c60f419e95f8549b90f0a8b422f2f0
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-23 11:11:53 -0600

    fixed sessions#create authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
 
-        if user && user.authenticate(password: params[:password])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             render json: UserSerializer.new(user)
         else
