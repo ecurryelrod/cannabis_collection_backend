e3e623765e731ce9dbc1a37dfe810a350125dddf
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-06-13 17:32:09 -0600

    added user_id to strain_params

--- app/controllers/api/v1/strains_controller.rb
+++ app/controllers/api/v1/strains_controller.rb
@@ -3,8 +3,15 @@ class Api::V1::StrainsController < ApplicationController
 
   def index
     @strains = Strain.all
+    
+    # if logged_in?
+      # @strains = current_user.strains
+      render json: StrainSerializer.new(@strains)
+    # else
+      # render json: {alert: "You must be logged in to see your experiences"}
+    # end
 
-    render json: StrainSerializer.new(@strains)
+    # render json: StrainSerializer.new(@strains)
     # render json: @strains
   end
 
@@ -15,6 +22,7 @@ class Api::V1::StrainsController < ApplicationController
 
   def create
     @strain = Strain.new(strain_params)
+    # @strain = current_user.strains.build(strain_params)
     # effect_ids gotten from createStrain action, sendableData variable.
     # by setting strain.effect_ids = to params[:effect_ids],
     # able to add effects to @strain.effects array due to model associations/join table.
@@ -51,6 +59,6 @@ class Api::V1::StrainsController < ApplicationController
     end
 
     def strain_params
-      params.require(:strain).permit(:name, :description, :terpene, :thc_amount, :cbd_amount, :cbg_amount, :type_id)
+      params.require(:strain).permit(:name, :description, :terpene, :thc_amount, :cbd_amount, :cbg_amount, :type_id, :user_id)
     end
 end
