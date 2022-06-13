0700c9ba4028a80d1a4a014b8be9077f6646506b
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-06-13 17:34:44 -0600

    updated UserSerializer for strain effects/type

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,17 +8,36 @@ class UserSerializer
   attributes :email, :name
   # has_many :strains
 
-  attribute :strains do |type|
-    type.strains.map do |strain| 
-        {
-          id: strain.id,
-          name: strain.name,
-          description: strain.description,
-          terpene: strain.terpene,
-          thc_amount: strain.thc_amount,
-          cbd_amount: strain.cbd_amount,
-          cbg_amount: strain.cbg_amount
+  attribute :strains do |user|
+    user.strains.map do |strain| 
+      {
+        id: strain.id,
+        name: strain.name,
+        description: strain.description,
+        terpene: strain.terpene,
+        thc_amount: strain.thc_amount,
+        cbd_amount: strain.cbd_amount,
+        cbg_amount: strain.cbg_amount,
+        effects: strain.effects.map do |effect| 
+            {
+              id: effect.id,
+              name: effect.name
+            }
+        end,
+        type: {
+          id: strain.type.id,
+          name: strain.type.name
         }
+      }
     end
   end
+
+  # attribute :effects do |strain|
+  #   strain.effects.map do |effect| 
+  #       {
+  #         id: effect.id,
+  #         name: effect.name
+  #       }
+  #   end
+  # end
 end
