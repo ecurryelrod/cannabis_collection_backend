730106cd9f30aa7b640f90823f8c541d5e4ac836
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-06-13 17:40:57 -0600

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_presence_of :name, :email
+    validates_uniqueness_of :email
 
     has_many :strains
     validates :email, uniqueness: true, presence: true
