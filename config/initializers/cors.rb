b0435d78e6b609324ed72045c71dd30cf8e794dd
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-07-03 15:27:03 -0600

    updated cors origins again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://cannabis-collection.herokuapp.com"
+    origins "http://localhost:3000/cannabis_collection_frontend"
 
     resource "*",
       headers: :any,
